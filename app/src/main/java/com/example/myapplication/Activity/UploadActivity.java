eb32e3661e76a73e9dc8bf67111d5af13fe544f7
TengYuanWenTai <2941912132@qq.com>
2022-09-26 12:09:06 +0800

    图片上传

--- app/src/main/java/com/example/myapplication/Activity/UploadActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/UploadActivity.java
@@ -1,16 +1,254 @@
 package com.example.myapplication.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.os.NetworkOnMainThreadException;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class UploadActivity extends AppCompatActivity {
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public File file ;
+    private ImageView bt_upload;
+    private ActivityResultLauncher<Intent> mResultLauncher;
+    private final Gson gson = new Gson();
+    private Context context;
+    private Uri picUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        bt_upload = findViewById(R.id.bt_upload);
+        bt_upload.setOnClickListener(this);
+        context = bt_upload.getContext();
+
+        //跳转带系统相册，选择图片并返回
+        mResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @RequiresApi(api = Build.VERSION_CODES.Q)
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK){
+                    Intent intent = result.getData();
+                    picUri = intent.getData();
+                    try {
+                        file = getFile(context,picUri);
+                        System.out.println(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (picUri != null){
+                        bt_upload.setImageURI(picUri);
+                        Log.d("binge","picUri"+picUri.toString());
+                        System.out.println("666");
+                        System.out.println(file);
+                        post(file);
+                    }
+                }
+
+            }
+        });
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_upload:
+                //跳转带系统相册，选择图片并返回
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                mResultLauncher.launch(intent);
+                break;
+        }
+    }
+
+    public static File getFile(Context context, Uri picUri) throws IOException {
+        File destinationFilename = new File(context.getFilesDir().getPath() + File.separatorChar + queryName(context, picUri));
+        try (InputStream ins = context.getContentResolver().openInputStream(picUri)) {
+            createFileFromStream(ins, destinationFilename);
+        } catch (Exception ex) {
+            Log.e("Save File", ex.getMessage());
+            ex.printStackTrace();
+        }
+        return destinationFilename;
+    }
+
+    public static void createFileFromStream(InputStream ins, File destination) {
+        try (OutputStream os = new FileOutputStream(destination)) {
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = ins.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+            os.flush();
+        } catch (Exception ex) {
+            Log.e("Save File", ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    private static String queryName(Context context, Uri uri) {
+        Cursor returnCursor =
+                context.getContentResolver().query(uri, null, null, null, null);
+        assert returnCursor != null;
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        String name = returnCursor.getString(nameIndex);
+        returnCursor.close();
+        return name;
+    }
+
+    private void post(File file){
+        new Thread(() -> {
+
+            // url路径
+            String url = "http://47.107.52.7:88/member/sign/image/upload";
+
+            // 请求头
+            Headers headers = new Headers.Builder()
+                    .add("appId", "7eb81bfee5854b769384f9c7532c701c")
+                    .add("appSecret", "541991e50804fd5d547b794ec8487ad94d1a0")
+                    .add("Accept", "application/json, text/plain, */*")
+                    .build();
+
+            MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
+            MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+
+            RequestBody fileBody = RequestBody.Companion.create(file, MEDIA_TYPE_JSON);
+            RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                    .addFormDataPart("file", file.getName(), fileBody)
+                    .build();
+
+            //请求组合创建
+            Request request = new Request.Builder()
+                    .url(url)
+                    // 将请求头加至请求中
+                    .headers(headers)
+                    .post(body)
+                    .build();
+            try {
+                OkHttpClient client = new OkHttpClient();
+                //发起请求，传入callback进行回调
+                client.newCall(request).enqueue(callback);
+            }catch (NetworkOnMainThreadException ex){
+                ex.printStackTrace();
+            }
+        }).start();
+    }
+
+    private final Callback callback = new Callback() {
+        @Override
+        public void onFailure(@NonNull Call call, IOException e) {
+            //TODO 请求失败处理
+            e.printStackTrace();
+        }
+        @Override
+        public void onResponse(@NonNull Call call, Response response) throws IOException {
+            //TODO 请求成功处理
+            Type jsonType = new TypeToken<ResponseBody<Object>>(){}.getType();
+            // 获取响应体的json串
+            String body = Objects.requireNonNull(response.body()).string();
+            Log.d("info", body);
+            // 解析json串到自己封装的状态
+            ResponseBody<Object> dataResponseBody = gson.fromJson(body,jsonType);
+            //Log.d("info", dataResponseBody.getData().toString());
+        }
+    };
+
+    /**
+     * http响应体的封装协议
+     * @param <T> 泛型
+     */
+    public static class ResponseBody <T> {
+
+        /**
+         * 业务响应码
+         */
+        private int code;
+        /**
+         * 响应提示信息
+         */
+        private String msg;
+        /**
+         * 响应数据
+         */
+        private T data;
+
+        public ResponseBody(){}
+
+        public int getCode() {
+            return code;
+        }
+        public String getMsg() {
+            return msg;
+        }
+        public T getData() {
+            return data;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "ResponseBody{" +
+                    "code=" + code +
+                    ", msg='" + msg + '\'' +
+                    ", data=" + data +
+                    '}';
+        }
+    }
+
 }
